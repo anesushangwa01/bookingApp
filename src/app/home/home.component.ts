8dd50f25183db033dcfda6ed841312ba7a58e57a
Anesu shangwa <97034450+Anesushangwa@users.noreply.github.com>
2024-07-18 11:30:06 +0200

    new

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,44 +15,18 @@ import { GoogleAuthComponent } from '../google-auth/google-auth.component';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  // constructor(private auth:  BookingService) { }
-  // auths: any[] = [];
-
-
-  // user: any;
-
- 
-  // ngOnInit() {
-  //   this.auth.user$.subscribe(user => {
-  //     this.user = user;
-  //   });
-  //   this.auth.isAuthenticated().subscribe();
-  // }
+  constructor(private auth:  BookingService) { }
+  auths: any[] = [];
 
 
   user: any;
-  isLoading = true;
-  errorMessage: string | null = null;
 
-  constructor(private bookingService: BookingService) {}
-
-  ngOnInit(): void {
-    this.bookingService.user$.subscribe(
-      (userInfo) => {
-        if (userInfo) {
-          this.user = userInfo;
-          console.log('User info received:', this.user);
-        } else {
-          console.warn('No user info received.');
-        }
-        this.isLoading = false;
-      },
-      (error) => {
-        console.error('Error fetching user info:', error);
-        this.errorMessage = 'Error fetching user info';
-        this.isLoading = false;
-      }
-    );
+ 
+  ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.user = user;
+    });
+    this.auth.isAuthenticated().subscribe();
   }
 
 
